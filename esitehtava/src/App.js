81ab17af5c399338dfcca9391285390fec1e1faf
Rasmus Koskinen <36254113+Metris96@users.noreply.github.com>
2020-02-28 21:55:09 +0200

    Update App.js

--- esitehtava/src/App.js
+++ esitehtava/src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useReducer} from 'react';
 import Tweet from './tweet';
 
 
-var count = document.cookie;
+let count = document.cookie;
 if(count == "" || count == 0){
   document.cookie=" =20; expires=Thu, 18 Dec 2022 12:00:00 UTC; path=/";
 }
@@ -15,12 +15,11 @@ function App(){
  
   console.log("Keksin arvo on " + count)
   //const [count, setCount] = useState(20); // pelaajan pisteet
-  const [click, setClick] = useState(0); // klikkaukset, muista vaihtaa, niin että hakee tiedostosta
+  const [click, setClick] = useState(1); // klikkaukset, muista vaihtaa, niin että hakee tiedostosta
   const increment = () => {
   setClick (click + 1)
   console.log("tämä on " + click + " klikkaus")
-    count = count - 1;
-    console.log(count)
+    count = count -1;
     checkWin(click)
     document.cookie =count;
     count = document.cookie;
@@ -29,40 +28,39 @@ function App(){
     setClick (click + 1)
     console.log("tämä on " + click + " klikkaus")
       count = 20;
-      console.log(count)
       document.cookie =count;
       count = document.cookie;
     }
   
-  var muuttuja = 10 - click % 10; // montako klikkausta seuraavaan voittoon
+  var muuttuja = 10 -  click % 10 +1; // montako klikkausta seuraavaan voittoon
+  if(muuttuja === 11){
+      muuttuja = 1;
+  }
   
   function checkWin(click){
-    if(click % 500 == 0  && click != 0){ // jos klikkausluku on jaollinen 500 eli joka 500 klikkaus, niin annetaan voitto
-      count = count + 250;
-      console.log("saat 250 pistettä")
+    if(click % 500 === 0  && click != 0){ // jos klikkausluku on jaollinen 500 eli joka 500 klikkaus, niin annetaan voitto
+      
+    count = count + 250;
+    document.cookie = count;
       muuttuja = 10;
       alert("Voitit 250 pistettä!!!")
     }
-    else if(click % 100 == 0 && click != 0){
+    else if(click % 100 === 0 && click != 0){
       count += 40;
-      console.log("saat 40 pistettä")
+      document.cookie = count;
       muuttuja = 10;
       alert("Voitit 40 pistettä!!!")
     }
-    else if(click % 10 == 0  && click != 0){
-      count += 5;
-      console.log("saat 5 pistettä")
+    else if(click % 10 === 0  && click != 0){
       muuttuja = 10;
-
+      count += 5;
+      document.cookie = count;
       alert("Voitit 5 pistettä!!!")
     }
 
   }
-  function setCookie(count){
-    document.cookie = count;
-    count = document.cookie;
-  }
-  if(count == 0){
+  
+  if(count < 1){
     return(
       <div className="App">
         <p>voi rähmä, hävisit pelin. voit aloittaa udeelleen käynnistämällä sivun</p>
